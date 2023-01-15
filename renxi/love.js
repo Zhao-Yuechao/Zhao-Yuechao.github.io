b898a10b6aebd8dbe8286a83661d83939d52361b
Zhao-Yuechao <67886841+Zhao-Yuechao@users.noreply.github.com>
2023-01-15 16:55:16 +0800

    Add files via upload

--- renxi/love.js
+++ renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("宝贝 点开我的心", 23, 10);
+            ctx.fillText("啾啾小宝贝 请点击爱心~", 23, 10);
             ctx.restore();
         },
         clear: function() {
